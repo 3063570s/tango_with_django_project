50c177793b426390f96c3debb61d016f83f04d20
“Calvin <3063570s@student.gla.ac.uk>
2025-03-24 19:36:32 +0000

    chapter 6 changes, not working correctly

--- rango/models.py
+++ rango/models.py
@@ -7,10 +7,6 @@ class Category(models.Model):
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
     
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
-    
     class Meta:
         verbose_name_plural = 'categories'
         
